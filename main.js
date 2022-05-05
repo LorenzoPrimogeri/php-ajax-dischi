41440bd888591e833fc461f94ac8031b45bc0d83
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-05-05 17:01:48 +0200

    changed link but still same problem

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = new Vue({
         result: "",
     },
     mounted() {
-        axios.get("http://localhost:800/php-ajax-dischi/assets/server.php").then((response) => {
+        axios.get("http://localhost/php-ajax-dischi/assets/server.php").then((response) => {
             this.result = response;
             console.log(response);
         })
